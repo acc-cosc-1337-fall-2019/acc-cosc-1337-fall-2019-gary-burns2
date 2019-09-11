5061e0ad42ffc8cdc7eea41a349b4201cc2b77a1
Gary Burns <gary.burns2@g.austincc.edu>
2019-09-11 18:16:10 -0500

    Homework 2 finished, test cases added.

--- test/examples_test/02_module_test/02_module_tests.cpp
+++ test/examples_test/02_module_test/02_module_tests.cpp
@@ -2,6 +2,7 @@
 #include "catch.hpp"
 #include "if.h"
 #include "if_else.h"
+#include "switch.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -22,4 +23,14 @@ TEST_CASE("Test get generation")
 	REQUIRE(get_generation(1960) == "Baby Boomer");
 	REQUIRE(get_generation(1940) == "Silent Generation");
 	REQUIRE(get_generation(1776) == "Invalid Year");
+}
+
+TEST_CASE("Test menu function")
+{
+	REQUIRE(menu(1) == "Option 1");
+	REQUIRE(menu(2) == "Option 2");
+	REQUIRE(menu(3) == "Option 3");
+	REQUIRE(menu(4) == "Option 4");
+	REQUIRE(menu(100) == "Invalid");
+
 }
