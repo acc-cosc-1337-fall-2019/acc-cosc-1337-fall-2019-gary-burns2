b81cae46441ef353fcd682431296bc66b7554d97
Gary Burns <gary.burns2@g.austincc.edu>
2019-09-11 15:57:06 -0500

    Homework 1 completed, test cases used.

--- src/homework/01_expressions/main.cpp
+++ src/homework/01_expressions/main.cpp
@@ -1,7 +1,11 @@
 ﻿//write include statements
-
+#include "expressions.h"
+#include <string>
+#include <iostream>
 
 //write namespace using statement for cout
+
+using std::cout;
 
 
 /*
@@ -9,7 +13,8 @@ Call multiply_numbers with 10 and 10 parameter values and display function resul
 */
 int main()
 {
-	
-
+	int product = multiply_numbers(10, 10);
+	cout << "The product is " << product;
+	cout << product;
 	return 0;
 }
