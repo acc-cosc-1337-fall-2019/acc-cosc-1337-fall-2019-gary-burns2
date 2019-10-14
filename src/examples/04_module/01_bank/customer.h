71e2af296572474f69e5949a5110a991c4f6ebe2
Gary Burns <gary.burns2@g.austincc.edu>
2019-10-14 18:48:33 -0500

    "Check in before changes"

--- src/examples/04_module/01_bank/customer.h
+++ src/examples/04_module/01_bank/customer.h
@@ -1,1 +1,20 @@
 //customer.h
+#include<vector>
+#include "bank_account.h"
+#include<iostream>
+
+using std::vector;
+using std::ostream;
+
+class Customer
+{
+public:
+	void add_account(BankAccount act);
+	friend ostream& operator<<(ostream& out, const Customer& act);
+	void remove_account(BankAccount act);
+
+private:
+	vector<BankAccount> accounts;
+	int total_balance{ 0 };
+
+};
